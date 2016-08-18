2afee3bef6bd63dfeb2c5362bb2395f7174d26cb
Jdonee <jdonee.zeng@gmail.com>
2016-08-18 18:04:22 +0800

    1、初始化项目

---
+++ src/main/java/org/jdonee/cooking/ShiroRedisExampleBoot.java
@@ -0,0 +1,12 @@
+package org.jdonee.cooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroRedisExampleBoot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiroRedisExampleBoot.class, args);
+	}
+}
